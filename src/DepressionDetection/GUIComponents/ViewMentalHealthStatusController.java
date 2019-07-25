70ef19c00e9f3e5ae9bfd2eb16ddf6cac73e35f9
JOMADI <victorjo603@gmail.com>
2019-07-25 02:00:49 +0100

    Add ViewMentalHealthStatus UI implementation.

--- src/DepressionDetection/GUIComponents/ViewMentalHealthStatusController.java
+++ src/DepressionDetection/GUIComponents/ViewMentalHealthStatusController.java
@@ -7,7 +7,14 @@ package DepressionDetection.GUIComponents;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,12 +23,35 @@ import javafx.fxml.Initializable;
  */
 public class ViewMentalHealthStatusController implements Initializable {
 
+
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Pane backArrowPane;
+
+    @FXML
+    private Pane closePane;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Text userName;
+
+    @FXML
+    private AreaChart<?, ?> areaChart;
+
+    @FXML
+    private Text healthReportTitle;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
